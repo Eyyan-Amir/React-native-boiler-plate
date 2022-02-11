901783ca5821fd693878977584ae21871dc7857b
ayyan <ayyanamir6@gmail.com>
2022-02-11 13:30:30 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello Ayyan</Text>
+    </View>
+  );
+}
+
+export default App;
